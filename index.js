bdb4ab4e5f8aa927b0b2af6e1232d879890bf7bd
chencheng <sorrycc@gmail.com>
2015-03-12 10:48:00 +0800

    chore: update index

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+exports.build = require('./lib/build');
+exports.getArgs = require('./lib/getArgs');
+exports.getFiles = require('./lib/getFiles');
+exports.logArgs = require('./lib/logArgs');
+exports.SPMModuleParser = require('./lib/SPMModuleParser');
+exports.SPMWebpackPlugin = require('./lib/SPMWebpackPlugin');
