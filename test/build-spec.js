b57ba89395cbd8ff23cf3d5dffda2035ce03856a
sorrycc <sorrycc@gmail.com>
2015-07-09 11:40:03 +0800

    testcase: entry can be required

--- test/build-spec.js
+++ test/build-spec.js
@@ -298,6 +298,15 @@ describe('lib/build.js', function() {
     assert(dest, 'css-output');
   });
 
+  it('require-entry', function*() {
+    yield build({
+      debug: true,
+      cwd: join(fixtures, 'require-entry'),
+      dest: dest
+    });
+    assert(dest, 'require-entry');
+  });
+
   it('require-node-modules', function*() {
     yield build({
       debug: true,
