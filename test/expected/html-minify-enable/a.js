8cc37a9350f11b1a10409f9cc7f42093d8afd343
玄寂 <xuanji.jw@taobao.com>
2015-07-14 12:16:46 +0800

    chore: fix testcase

--- test/expected/html-minify-enable/a.js
+++ test/expected/html-minify-enable/a.js
@@ -1,1 +1,1 @@
-!function(r){function t(n){if(o[n])return o[n].exports;var e=o[n]={exports:{},id:n,loaded:!1};return r[n].call(e.exports,e,e.exports,t),e.loaded=!0,e.exports}var o={};return t.m=r,t.c=o,t.p="",t(0)}([function(r,t,o){r.exports=o(2)},function(r,t,o){},function(r,t,o){o(1),console.log(1)}]);
+!function(r){function t(n){if(o[n])return o[n].exports;var e=o[n]={exports:{},id:n,loaded:!1};return r[n].call(e.exports,e,e.exports,t),e.loaded=!0,e.exports}var o={};return t.m=r,t.c=o,t.p="",t(0)}([function(r,t,o){r.exports=o(2)},function(r,t){},function(r,t,o){o(1),console.log(1)}]);
